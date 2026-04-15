fd5fa370468469ee30b005fa6c47dd13055040bc
Leonardo <leomicale.lm@gmail.com>
2026-04-15 13:29:23 -0300

    Desarrollo metodo listar

--- ArticuloNegocio/ArticuloNegocio.cs
+++ ArticuloNegocio/ArticuloNegocio.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Dominio;
+using ConexionBD;
 
 namespace Negocio
 {
@@ -12,16 +13,35 @@ namespace Negocio
         public List<Articulo> listar()
         {
             List<Articulo> lista = new List<Articulo>();
+            AccesoDatos datos = new AccesoDatos();
 
             try
             {
-                
+                datos.setearConsulta("Select Id, Codigo, Nombre, Descripcion, Precio from ARTICULOS");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Articulo art = new Articulo();
+                    art.IdArticulo = (int)datos.Lector["Id"];
+                    art.Codigo = (string)datos.Lector["Codigo"];
+                    art.Nombre = (string)datos.Lector["Nombre"];
+                    art.Descripcion = (string)datos.Lector["Descripcion"];
+                    art.Precio = (decimal)datos.Lector["Precio"];
+
+                    lista.Add(art);
+                }
+
                 return lista;
             }
             catch (Exception ex)
             {
 
                 throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
             }
         }
         
--- TPWinForm/Form1.cs
+++ TPWinForm/Form1.cs
@@ -7,14 +7,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Dominio;
+using Negocio;
 
 namespace TPWinForm
 {
     public partial class Form1 : Form
     {
+        private List<Articulo> listaArticulo;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ArticuloNegocio negocio = new ArticuloNegocio();
+            try
+            {
+                listaArticulo = negocio.listar();
+                dgvArticulos.DataSource = listaArticulo;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
         }
     }
 }
