fd9199a432926646edf72e65a1625f7d456824cb
Leonardo <leomicale.lm@gmail.com>
2026-04-18 12:31:02 -0300

    Creación clase CategoriaNegocio y método listar

---
+++ ArticuloNegocio/CategoriaNegocio.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+using ConexionBD;
+
+namespace ArticuloNegocio
+{
+    internal class CategoriaNegocio
+    {
+        public List<Categoria> listar()
+        {
+            List<Categoria> lista = new List<Categoria>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("Select IdCategoria, Descripcion from Categorias");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Categoria cat = new Categoria();
+                    cat.IdCategoria = (int)datos.Lector["Id"];
+                    cat.Descripcion = (string)datos.Lector["Descripcion"];
+
+                    lista.Add(cat);
+                }
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+}
